cbf93ecc5b90f457a281d188441b8ee5f05a8b1f
Adolf David <143256974+adolfzcoder@users.noreply.github.com>
2024-09-21 12:33:01 +0200

    Errors resolved, code works(kinda)
    Doesnt verifying user

--- src/Login.java
+++ src/Login.java
@@ -3,10 +3,13 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Login {
-    
+
+
     public static void login(String userInfo) throws FileNotFoundException{
         File file = new File("src\\emails-passwords.txt");
+
         Scanner scan = new Scanner(file);
+
         String regex = "[:]";
         System.out.println(userInfo);
         String[] userInfoCredentials = userInfo.split(regex);
@@ -15,32 +18,39 @@ public class Login {
         String userEmail = userInfoCredentials[0];
         String userPassword = userInfoCredentials[1];
 
+        boolean isAuthenticated = userAuthentication(scan, userEmail, userPassword, regex);
         
-        
+
+        if (isAuthenticated){
+            System.out.println("Succesfully logged in");
+        }
     }
 
-        public static boolean userAuthentication(userEmail,userPassword) {
-                boolean isLoggedIn = false;
-                while (scan.hasNextLine()) {
-            
-                    
-                    // Read and print each line
-                    String line = scan.nextLine();
-                    
-                    String[] credentials = line.split(regex);
-                    
-                    // Store the email and password in separate variables
-                    String emailFromFile = credentials[0];
-                    String passwordFromFile = credentials[1];
+    public static  boolean userAuthentication(Scanner scan, String userEmail, String userPassword, String regex) {
+        boolean isLoggedIn = false;
+        while (scan.hasNextLine()) {
     
-                    if (userEmail.equals(emailFromFile) && userPassword.equals(passwordFromFile)) {
-                        System.out.println("User is successfully logged in");
-                        isLoggedIn = true;
-                    }
-                    else{
-                        System.out.println("Wrong email or password!");
-                        isLoggedIn = false;
-                    }
-                }
+            
+            // Read and print each line
+            String line = scan.nextLine();
+            
+            String[] credentials = line.split(regex);
+            
+            // Store the email and password in separate variables
+            String emailFromFile = credentials[0];
+            String passwordFromFile = credentials[1];
+
+            if (userEmail.equals(emailFromFile) && userPassword.equals(passwordFromFile)) {
+                System.out.println("User is successfully logged in");
+                isLoggedIn = true;
             }
+            else{
+                System.out.println("Wrong email or password!");
+                isLoggedIn = false;
+            }
+            
+        }
+        return isLoggedIn;
+    }
+        
 }
