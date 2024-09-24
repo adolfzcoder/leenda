b5385bd9d1c8e3f5eae51ab0a91284a32a768cd6
Adolf David <143256974+adolfzcoder@users.noreply.github.com>
2024-09-24 23:29:58 +0200

    Added Car owner gui

---
+++ src/ownermodules/src/ownermodules/Ownermodules.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ownermodules;
+
+import javax.swing.UnsupportedLookAndFeelException;
+
+/**
+ *
+ * @author adolf
+ */
+public class Ownermodules {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws UnsupportedLookAndFeelException {
+        // TODO code application logic here
+        new OwnerDashboard();
+    }
+    
+}
